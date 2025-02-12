e496db2da228bde7087ffd4db0eafa39075942a4
Calvin Dudek <calvindudek@googlemail.com>
2025-02-12 19:09:38 +0100

    changed logo to white

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -23,7 +23,7 @@ export const Logo = (props: Props) => {
       fetchPriority={priority}
       decoding="async"
       className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://storage.cloud.google.com/forwardlabs.ch/storage/media/forward-logo.svg"
+      src="https://storage.cloud.google.com/forwardlabs.ch/storage/media/forward-logo-white.svg"
       // src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
     />
   )
