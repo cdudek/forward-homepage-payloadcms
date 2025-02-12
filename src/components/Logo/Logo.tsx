080cb3b06be038160455783649350082830bda98
Calvin Dudek <calvindudek@googlemail.com>
2025-02-12 22:45:18 +0100

    updated logo url, didnt work somehow

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -23,7 +23,7 @@ export const Logo = (props: Props) => {
       fetchPriority={priority}
       decoding="async"
       className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://storage.cloud.google.com/forwardlabs.ch/storage/media/forward-logo-white.svg"
+      src="https://storage.googleapis.com/forwardlabs.ch/storage/media/forward-logo-white.svg"
     />
   )
 }
