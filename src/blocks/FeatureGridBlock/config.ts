47f414e7f7efb7f1504ca9a99651360f2bd49e9a
Calvin Dudek <calvindudek@googlemail.com>
2025-03-12 02:26:48 +0100

    alles laeuft

--- src/blocks/FeatureGridBlock/config.ts
+++ src/blocks/FeatureGridBlock/config.ts
@@ -201,7 +201,6 @@ export const FeatureGridBlock: Block = {
                       ]
                     },
                   }),
-                  label: false,
                 },
               ],
             },
