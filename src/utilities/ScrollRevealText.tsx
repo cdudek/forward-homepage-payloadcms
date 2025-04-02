79a681d855dca7a32f5510a8979006b6569cdaf3
Calvin Dudek <calvindudek@googlemail.com>
2025-04-02 09:18:58 +0200

    fixed coloured text

--- src/utilities/ScrollRevealText.tsx
+++ src/utilities/ScrollRevealText.tsx
@@ -54,8 +54,8 @@ export const ScrollRevealText: React.FC<ScrollRevealTextProps> = ({
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: element,
-          start: 'top 30%',
-          end: 'bottom 80%',
+          start: 'top center',
+          end: 'bottom center',
           scrub: 1,
           toggleActions: 'play none none reverse',
         },
