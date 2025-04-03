8c6c8e9b1c2dd8ab40086dca5b33340496450b75
Calvin Dudek <calvindudek@googlemail.com>
2025-04-04 00:42:25 +0200

    service accordion

--- src/blocks/ServicesAccordionBlock/Component.tsx
+++ src/blocks/ServicesAccordionBlock/Component.tsx
@@ -220,9 +220,7 @@ export const ServicesAccordionBlock: React.FC<ServicesAccordionBlockProps> = ({
               <div className="flex flex-col justify-center">
                 <div className="flex w-full flex-col gap-6 text-center lg:text-left">
                   <div className="prose prose-sm md:prose-base lg:prose-lg">
-                    <h2 className="m-0 bg-none p-0 text-4xl font-bold leading-tight lg:text-5xl">
-                      {formattedTitle}
-                    </h2>
+                    <h2 className="m-0 bg-none p-0 leading-tight">{formattedTitle}</h2>
                   </div>
 
                   {/* Button - Only visible on desktop */}
