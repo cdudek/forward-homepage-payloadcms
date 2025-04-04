82134a3dac4048880b662413ce63818c335dee54
Calvin Dudek <calvindudek@googlemail.com>
2025-04-04 13:31:05 +0200

    cloudflare vercel optimisations

--- next.config.js
+++ next.config.js
@@ -19,9 +19,50 @@ const nextConfig = {
         }
       }),
     ],
+    domains: ['imagedelivery.net'],
   },
   reactStrictMode: true,
+  output: 'standalone',
+  compress: true,
+  http2: true,
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ['payload'],
+  },
   redirects,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default withPayload(nextConfig)
